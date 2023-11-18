95ac373e18d09b0d444fd92d5e8ddca785396883
mithridatem <72612782+mithridatem@users.noreply.github.com>
2023-11-18 13:21:47 +0100

    Factorisation du code

--- main.js
+++ main.js
@@ -35,52 +35,25 @@ function distance(){
     }
 }
 function distance2(){
+    //récupération de la zone de résultat
+    const resultat = document.querySelector('#resultat');
     //récupération des villes
     const villeD = document.querySelector("#ville_d").value;
-    let latDepart;
-    let longDepart;
     const villeA = document.querySelector("#ville_a").value;
-    let latArrive;
-    let longArrive;
+    //base url
     const url = "https://api-adresse.data.gouv.fr/search/?q=";
     //test siles champs ville ne sont pas tous remplis
-    if(villeA == "" || villeA == ""){
+    if(villeD == "" || villeA == ""){
         resultat.textContent = "Veuillez renseigner toutes les valeurs";
     }
     //récupération des coordonnées de la ville de départ
-    const recupCoordVilleD = fetch(url+villeD).then(async response=>{
-        const data = await response.json();
-        //test si la ville n'existe pas
-        if(data.features== ""){
-            latDepart = "vide";
-            longDepart = "vide";
-        }
-        //test si la ville existe
-        else{
-            latDepart= data.features[0].geometry.coordinates[0];
-            longDepart= data.features[0].geometry.coordinates[1];
-        }
-    });
-    //récupération des coordonnées de la ville d'arrivé
-    const recupCoordVilleA = fetch(url+villeA).then(async response=>{
-        const data = await response.json();
-        //test si la ville n'existe pas
-        if(data.features== ""){
-            latArrive = "vide";
-            longArrive = "vide";
-        }
-        //test si la ville existe
-        else{
-            latArrive= data.features[0].geometry.coordinates[0];
-            longArrive= data.features[0].geometry.coordinates[1];
-        }
-    });
+    const recupCoordVilleD = apiVille(url+villeD);
+    //récupération des coordonnées de la ville d'arrivée
+    const recupCoordVilleA = apiVille(url+villeA);
     //lancement du calcul de distance
     setTimeout(()=>{
-        //récupération de la zone de résultat
-        const resultat = document.querySelector('#resultat');
         //test les 2 villes n'existent pas
-        if(latDepart == "vide" && longDepart == "vide" && latArrive == "vide" && longArrive == "vide"){
+        if(recupCoordVilleD[0] == "vide" && recupCoordVilleA[0] == "vide"){
             document.querySelector("#ville_d").value = "";
             document.querySelector("#ville_a").value = "";
             document.querySelector("#ville_d").placeholder = `${villeD} n'existe pas`;
@@ -88,13 +61,13 @@ function distance2(){
             resultat.textContent = "";
         }
         //test la ville de départ n'existe pas
-        else if(latDepart == "vide" && longDepart == "vide"){
+        else if(recupCoordVilleD[0] == "vide" && recupCoordVilleD[1] == "vide"){
             document.querySelector("#ville_d").value = "";
             document.querySelector("#ville_d").placeholder = `${villeD} n'existe pas`;
             resultat.textContent = "";
         }
         //test la ville d'arrivé n'existe pas
-        else if(latArrive == "vide" && longArrive == "vide"){
+        else if(recupCoordVilleA[0] == "vide" && recupCoordVilleA[1] == "vide"){
             document.querySelector("#ville_a").value = "";
             document.querySelector("#ville_a").placeholder = `${villeA} n'existe pas`;
             resultat.textContent = "";
@@ -103,24 +76,39 @@ function distance2(){
         else{
             document.querySelector("#ville_d").value = "";
             document.querySelector("#ville_a").value = "";
-            document.querySelector("#ville_d").placeholder = "";
-            document.querySelector("#ville_a").placeholder = "";
-            //facteur d'échelle
-            const factE = 1.852;
-            //test si les champs sont tous remplis
-            if(villeD!=""&&villeA!=""){
-                //calcul angle x en deg
-                const xDeg = Math.abs(latDepart-latArrive)*Math.cos((longDepart+longArrive)/2);
-                //calcul angle y en deg
-                const yDeg = Math.abs(longDepart-longArrive);
-                //calcul de la distance en deg
-                const distDeg = Math.sqrt((xDeg**2)+(yDeg**2));
-                //conversion en kilométres
-                const distance = (factE*60*distDeg).toFixed(2);
-                //Affichage du résultat
-                resultat.textContent = "La distance est de : "+distance+" km";
-                resultat.style.color = "green";
-            }
+            document.querySelector("#ville_d").placeholder = `${villeD}`;
+            document.querySelector("#ville_a").placeholder = `$${villeA}`;
+            //Affichage du résultat
+            resultat.textContent = "La distance est de : "+calcul(recupCoordVilleD[0], recupCoordVilleD[1], recupCoordVilleA[0], recupCoordVilleA[1])+" km";
+            resultat.style.color = "green";
         }
     }, 500);
+}
+function calcul(latDepart, longDepart, latArrive, longArrive){
+    //facteur d'échelle
+    const factE = 1.852;
+    //calcul angle x en deg
+    const xDeg = Math.abs(latDepart-latArrive)*Math.cos((longDepart+longArrive)/2);
+    //calcul angle y en deg
+    const yDeg = Math.abs(longDepart-longArrive);
+    //calcul de la distance en deg
+    const distDeg = Math.sqrt((xDeg**2)+(yDeg**2));
+    //conversion en kilométres
+    const distance = (factE*60*distDeg).toFixed(2);
+    return distance;
+}
+function apiVille(url){
+    let tabVille = [];
+    fetch(url).then(async response=>{
+        const data = await response.json();
+        //test si la ville n'existe pas
+        if(data.features== ""){
+            tabVille.push("vide","vide");
+        }
+        //test si la ville existe
+        else{
+            tabVille.push(data.features[0].geometry.coordinates[0], data.features[0].geometry.coordinates[1]);
+        }
+    });
+    return tabVille;
 }
